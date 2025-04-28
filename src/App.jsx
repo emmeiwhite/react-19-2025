d64ac60ac6b582543a1d5937f2bc2d998767c7ef
emmeiwhite <emmeiwhite@gmail.com>
2025-04-28 18:01:31 +0530

    react-2025: thinking in terms of components

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Title from './components/Title'
+import Todo from './components/Todo'
+
+function App() {
+  return (
+    <>
+      <Title />
+      <Todo />
+      <Todo />
+      <Todo />
+      <Todo />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Todo.jsx
@@ -0,0 +1,9 @@
+const Todo = () => {
+  return (
+    <div className="todo">
+      <h2>Finish React Crash Course</h2>
+      <button>Delete</button>
+    </div>
+  )
+}
+export default Todo
