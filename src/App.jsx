bdb85b324ecaecf6af0ded45b450936c61a79888
emmeiwhite <emmeiwhite@gmail.com>
2025-04-29 01:19:36 +0530

    react-2025 reactive variable declaration with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { useState } from 'react'
 import './App.css'
 import Popup from './components/Popup'
 import Title from './components/Title'
 import Todo from './components/Todo'
 
 function App() {
+  // Reactive variable declared with useState --- When a reactive variable is updated with reactive function --- the component re-renders
+  const [isPopupOpen, setIsPopupOpen] = useState(false)
+  /* --
   // A normal variable
   let isPopupOpen = false
 
@@ -13,6 +17,7 @@ function App() {
     isPopupOpen = !isPopupOpen
     console.log(isPopupOpen)
   }
+  -- */
   const handleChange = e => {
     console.log(e.target.value)
   }
@@ -25,7 +30,7 @@ function App() {
           onChange={handleChange}
           style={{ padding: '0.25rem 1rem' }}
         />
-        <button onClick={togglePopup}>add item</button>
+        <button onClick={() => setIsPopupOpen(true)}>add item</button>
       </div>
 
       <Todo
